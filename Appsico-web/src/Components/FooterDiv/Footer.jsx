6895eb1e49b319c8c2f020f17d65d8674980059e
DaniloBF10 <danilo.borim@granostudio.com.br>
2023-09-26 00:12:25 -0300

    comitao

--- Appsico-web/src/Components/FooterDiv/Footer.jsx
+++ Appsico-web/src/Components/FooterDiv/Footer.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
 
+import {AiFillInstagram} from 'react-icons/ai'
+import {BsFacebook} from 'react-icons/bs'
+import {AiOutlineTwitter} from 'react-icons/ai'
+
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid grid-cols-5 m-auto items-start justify-center'>
+      <div>
+        <div className="logoDiv">
+          <h1 className="logo text-[25px] text-white pb-[1.5rem]">
+            <strong>App</strong>sico
+          </h1>
+        </div>
+        <p className='text-white pb-[13px] opacity-70 leading-7'>
+          <strong>App</strong>sico is a platform that allows you to manage your apps.
+        </p>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Sobre Nós</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Funções</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Notícias</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>FAQ</li>
+
+        </div>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Sobre Nós</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Funções</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Notícias</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>FAQ</li>
+
+        </div>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Sobre Nós</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Funções</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>Notícias</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1]'>FAQ</li>
+
+        </div>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div>
+          <small className='text-[14px] text-white'>
+            matstank@hotmail.com
+          </small>
+          <div className='icons flex gap-4 py-[1rem]'>
+            <AiFillInstagram className='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor' />
+            <BsFacebook className='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor' />
+            <AiOutlineTwitter className='bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blueColor' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- Appsico-web/src/Components/ValueDiv/Value.jsx
+++ Appsico-web/src/Components/ValueDiv/Value.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import simple from '../../Assets/simple.jpg'
+import simple from '../../Assets/simple.png'
 
 const Value = () => {
   return (
@@ -9,13 +9,71 @@ const Value = () => {
       </h1>
 
       <div className='grid gap-[10rem] grid-cols-3 items-center'>
+       
         <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
           <div className='flex items-center gap-3'>
-            <div className='imgDiv p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
-              <img src={simple} alt="" />
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={simple} alt="" className='w-[70%]' />
             </div>
+
+            <span className='font-semibold text-textColor text-[18px]'>
+              Acolhimento
+            </span>
           </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem]'>
+            Acolhimento é um dos mais importantes aspectos da mente. É o que
+            acontece quando a mente está em um estado de acordo com o seu
+            comportamento.
+          </p>
         </div>
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={simple} alt="" className='w-[70%]' />
+            </div>
+
+            <span className='font-semibold text-textColor text-[18px]'>
+              Acolhimento
+            </span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem]'>
+            Acolhimento é um dos mais importantes aspectos da mente. É o que
+            acontece quando a mente está em um estado de acordo com o seu
+            comportamento.
+          </p>
+        </div>
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={simple} alt="" className='w-[70%]' />
+            </div>
+
+            <span className='font-semibold text-textColor text-[18px]'>
+              Acolhimento
+            </span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem]'>
+            Acolhimento é um dos mais importantes aspectos da mente. É o que
+            acontece quando a mente está em um estado de acordo com o seu
+            comportamento.
+          </p>
+        </div>
+
+      </div>
+
+      <div className="card mt-[2rem] flex justify-between bg-blueColor p-[5rem] rounded-[10px]">
+
+        <div>
+          <h1 className='text-blueColor text-[30px] font-bold'>Pronto para escolher um Psicólogo?</h1>
+          <h2 className='text-textColor text-[25px] font-bold'>Vamos começar!</h2>
+        </div>
+          
+          <button className='border-[2px] rounded-[10px] py-[4px] px-[50px] text-[18px] font-semibold text-blueColor hover:bg-white border-blueColor'>
+            Comece Agora
+          </button>
+        
       </div>
     </div>
   )
