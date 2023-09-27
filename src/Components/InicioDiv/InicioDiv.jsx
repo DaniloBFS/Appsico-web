4885b4ac3c94ead5abfcba3e4e2c896fc09c3a35
DaniloBF10 <danilo.borim@granostudio.com.br>
2023-09-27 16:22:27 -0300

    comitao

--- src/Components/InicioDiv/InicioDiv.jsx
+++ src/Components/InicioDiv/InicioDiv.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Inicio = () => {
   return (
       <h1 className='text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block'>
-        Seja bem vindo(a) ao Appsico!
+        Seja bem vindo(a) <br /> ao Appsico!
       </h1>
   )
 }
