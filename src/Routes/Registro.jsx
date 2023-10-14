05099cc9825c3e7c273706997ddc2fb45d845e47
DaniloBF10 <danilo.borim@granostudio.com.br>
2023-10-14 16:14:37 -0300

    comitao login

--- src/Routes/Registro.jsx
+++ src/Routes/Registro.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Registroform from "../Components/FormRegistro/FormRegistro.jsx";
 
 const Registro = () => {
     return (
-        <div>
-            <h1>Registre-se</h1>
-        </div>
+       <div className="flex w-full h-screen mt-20 mb-20" >
+            <div className="w-full flex items-center justify-center lg:w-1/2">
+                <Registroform />
+            </div>
+            <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-white-200">
+                <div className="w-60 h-60 bg-gradient-to-tr from-blue-500 to-violet-500 rounded-full animate-spin"/>
+                <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" />
+            </div>
+       </div>
     )
   }
   
