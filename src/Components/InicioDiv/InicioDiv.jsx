efcdb7159154f88b2538fc0d1315b6a83916be44
DaniloBF10 <danilo.borim@granostudio.com.br>
2023-11-22 15:49:35 -0300

    comitao

--- src/Components/InicioDiv/InicioDiv.jsx
+++ src/Components/InicioDiv/InicioDiv.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const Inicio = () => {
   return (
@@ -10,7 +10,7 @@ const Inicio = () => {
               </h1>
              
               <button className='border-[2px] rounded-[10px] py-[4px] px-[50px] text-[18px] font-semibold text-blueColor hover:bg-greyIsh border-blueColor'>
-                <a href="/Psicologos"> Comece Agora</a>
+              <Link to="/Psicologos"> Comece Agora</Link>
               </button>
             </div>
             <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-white-200">
